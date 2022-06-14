12ba08c885731cc7ead5e700d6fe89d769753dcd
sooyun <55366664+imcodding@users.noreply.github.com>
2022-06-15 08:14:34 +0900

    리터럴

--- src/main/java/com/thymeleaf/basic/BasicController.java
+++ src/main/java/com/thymeleaf/basic/BasicController.java
@@ -68,6 +68,12 @@ public class BasicController {
         model.addAttribute("param2", "data2");
         return "basic/link";
     }
+
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
