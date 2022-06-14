2b62b7f2163d70aba676f0da69cac5ef3e1877c7
sooyun <55366664+imcodding@users.noreply.github.com>
2022-06-14 13:10:29 +0900

    유틸리티 객체와 날짜

--- src/main/java/com/thymeleaf/basic/BasicController.java
+++ src/main/java/com/thymeleaf/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -55,6 +56,11 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
