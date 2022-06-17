cdd6a20e03bcb4d6ebfe90df994866358060b445
sooyun <55366664+imcodding@users.noreply.github.com>
2022-06-17 12:39:14 +0900

    타입리프 스프링 통합
    *화면 및 기능 추가
    - 상품 조회/등록/수정

---
+++ src/main/java/com/thymeleaf/TestDataInit.java
@@ -0,0 +1,21 @@
+package com.thymeleaf;
+
+import com.thymeleaf.form.domain.item.Item;
+import com.thymeleaf.form.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
